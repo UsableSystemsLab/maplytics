70a590b0bae499f1eb718a1bd9ca1db7dfe21681
Sultan <sultanbinyahb@gmail.com>
2025-10-19 23:12:07 +0300

    Refactor header semantics and add HeroSection component
    Updated the header structure to improve semantic HTML:
    - Replaced multiple <h1> tags with <nav> and <ul> <li> elements.
    - Changed navigation items to <a> tags for accessibility.
    - Added alt text to the logo image.
    - Adjusted the layout for the Header component for better alignment.
    - Added HeroSection component to page.js

--- nextjs_app/app/page.js
+++ nextjs_app/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Header from "../components/Header"
-
+import HeroSection from "../components/HeroSection";
 export default function Home() {
   return (
     <div className="">
       <Header />
+      <HeroSection />
     </div>
   );
 }
--- nextjs_app/components/Header.jsx
+++ nextjs_app/components/Header.jsx
@@ -2,12 +2,25 @@ import { Button } from "./ui/button"
 
 export default function Header() {
     return (
-        <header className="w-full flex items-center justify-center gap-20 py-4 px-8 bg-white">
-            <img src="/logo.svg" className="w-44" />
-            <h1 className="text-2xl hover: cursor-pointer">Home</h1>
-            <h1 className="text-2xl hover: cursor-pointer">About Us</h1>
-            <h1 className="text-2xl hover: cursor-pointer">Contact Us</h1>
-            <Button size="lg" className={"text-base hover: cursor-pointer"}>Sign in</Button>
+        <header className="w-full h-36 flex justify-between gap-20 py-4 px-8 bg-white shadow-md items-center">
+            <img src="/logo.svg" alt="MAPLYTICS logo" className="w-52 ml-14" />
+            <nav className="flex items-center gap-16 flex-1 justify-end">
+                <ul className="flex gap-16 items-center">
+                    <li>
+                        <a href="#" className="text-4xl hover:cursor-pointer">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-4xl hover:cursor-pointer">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-4xl hover:cursor-pointer">Contact Us</a>
+                    </li>
+                </ul>
+
+                <Button size="lg" className="btn-header w-48 h-14 text-2xl font-semibold hover:cursor-pointer ml-10">
+                    Sign in
+                </Button>
+            </nav>
         </header>
     )
 }
