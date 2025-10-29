cd1d0ba1b40e0d4ca2b6c69b7e055366c70e257f
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2025-10-29 16:33:34 +0300

    initialize next-intl internationalization

--- nextjs_app/app/[locale]/page.js
+++ nextjs_app/app/[locale]/page.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import Header from "../components/Header"
-import HeroSection from "../components/HeroSection";
-import FeatureOverview from "../components/FeatureOverview";
-import Footer from "../components/Footer";
+import Header from "../../components/Header"
+import HeroSection from "../../components/HeroSection";
+import FeatureOverview from "../../components/FeatureOverview";
+import Footer from "../../components/Footer";
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
