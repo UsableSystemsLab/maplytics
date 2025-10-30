547bdac4e6f231cc18e3fdbda6c3400dcbde2d3e
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2025-10-30 16:12:13 +0300

    i18n: apply internationalization on landing page and footer

--- nextjs_app/components/Footer.jsx
+++ nextjs_app/components/Footer.jsx
@@ -1,11 +1,15 @@
+import { useTranslations } from 'next-intl';
+
 export default function Footer() {
+    const t = useTranslations('footer');
+
     return (
-        <footer className="w-full  py-8 mt-32 flex flex-col items-center bg-white shadow-lg">
-            <p className="text-gray-600 text-lg">© 2025 Maplytics. All rights reserved.</p>
+        <footer className="w-full py-8 mt-32 flex flex-col items-center bg-white shadow-lg">
+            <p className="text-gray-600 text-lg">© {new Date().getFullYear()} {t('copyright')}</p>
             <div className="mt-4 flex space-x-6">
-                <a href="/privacy" className="text-gray-600 hover:text-gray-800 transition-colors duration-300">Privacy Policy</a>
-                <a href="/terms" className="text-gray-600 hover:text-gray-800 transition-colors duration-300">Terms of Service</a>
-                <a href="/contact" className="text-gray-600 hover:text-gray-800 transition-colors duration-300">Contact Us</a>
+                <a href="/privacy" className="text-gray-600 hover:text-gray-800 transition-colors duration-300">{t('privacyPolicy')}</a>
+                <a href="/terms" className="text-gray-600 hover:text-gray-800 transition-colors duration-300">{t('termsOfService')}</a>
+                <a href="/contact" className="text-gray-600 hover:text-gray-800 transition-colors duration-300">{t('contactUs')}</a>
             </div>
         </footer>
     )
