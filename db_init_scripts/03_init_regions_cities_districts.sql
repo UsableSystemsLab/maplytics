e91a2d908b75e4b391240fec33f519cd03c945da
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2026-02-17 19:09:10 +0300

    update districts table with polygon geometries

--- db_init_scripts/03_init_regions_cities_districts.sql
+++ db_init_scripts/03_init_regions_cities_districts.sql
@@ -1,7 +1,7 @@
 -- Data Source
 -- https://github.com/homaily/Saudi-Arabia-Regions-Cities-and-Districts
 
-
+-- Regions Table
 CREATE TABLE regions (
   region_id SERIAL PRIMARY KEY,
   capital_city_id INTEGER NOT NULL,
@@ -27,9 +27,12 @@ CREATE INDEX cities_center_gix
 
 -- Districts Table
 CREATE TABLE districts (
-  district_id VARCHAR(12) PRIMARY KEY,
-  city_id INTEGER NOT NULL,
-  region_id INTEGER NOT NULL,
-  name_ar VARCHAR(64) NOT NULL DEFAULT '',
-  name_en VARCHAR(64) NOT NULL DEFAULT ''
+    boundaries geometry(Polygon, 4326),
+    city_id integer,
+    district_id bigint,
+    name_ar text,
+    name_en text,
+    region_id integer
 );
+
+CREATE INDEX districts_boundaries_idx ON districts USING GIST (boundaries);
