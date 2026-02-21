182bd838c63d180812b5d2e03fd442fd0a0d325c
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2026-02-22 00:22:43 +0300

    move file serving logic from local volume to S3

---
+++ api_server/controllers/files.controller.js
@@ -0,0 +1,7 @@
+export const getPublicFile = async (req, res) => {
+    res.status(501).json({ message: "getPublicFile not implemented yet" });
+};
+
+export const getPrivateFile = async (req, res) => {
+    res.status(501).json({ message: "getPrivateFile not implemented yet" });
+};
--- api_server/routes/files.routes.js
+++ api_server/routes/files.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import { getPublicFile, getPrivateFile } from '../controllers/files.controller.js';
 
 const router = express.Router();
 
-// Serve public datasets (organized by user ID)
-router.use('/public', express.static('/datasets/public'));
+// Serve public datasets (organized by user ID) - Fetched from S3
+router.get('/public/:userId/:filename', getPublicFile);
 
-// Serve private datasets (organized by project ID)
-router.use('/private', express.static('/datasets/private'));
+// Serve private datasets (organized by project ID) - Fetched from S3
+router.get('/private/:projectId/:filename', getPrivateFile);
 
 export default router;
+
