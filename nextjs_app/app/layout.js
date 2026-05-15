12cfd1d4b975eeef338073953fc23b90bff3ed44
Khalid Alharbi <kalharbi@users.noreply.github.com>
2026-05-15 22:57:31 +0300

    remove nested html and body tags from the locale layout

--- nextjs_app/app/layout.js
+++ nextjs_app/app/layout.js
@@ -1,36 +1,11 @@
-import { getTranslations } from "next-intl/server"
-import { defaultLocale } from "../i18n"
 import "./globals.css";
-import { Montserrat } from 'next/font/google';
-
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800'],
-  variable: '--font-montserrat',
-  display: 'swap',
-});
 
 export const viewport = {
   width: "device-width",
   initialScale: 1,
   themeColor: "#134565",
-}
-
-export async function generateMetadata() {
-  const t = await getTranslations({ locale: defaultLocale, namespace: "metadata" })
-
-  return {
-    title: t("title"),
-    description: t("description"),
-  }
-}
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html suppressHydrationWarning>
-      <body suppressHydrationWarning className={montserrat.className}>
-        {children}
-      </body>
-    </html>
-  );
+  return children;
 }
